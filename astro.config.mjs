eb354e69a0c4516e9b922f6234d5a3b39b257a45
shard7 <sh7user@gmail.com>
2025-05-13 18:59:42 +0200

    feat: custom styling and docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,92 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeBlack from "starlight-theme-black";
+import rehypeMermaid from "rehype-mermaid";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Malbox",
+      logo: {
+        dark: "./src/assets/malbox-logo-dark.svg",
+        light: "./src/assets/malbox-logo-light.svg",
+      },
+      customCss: ["./src/styles/global.css"],
+      sidebar: [
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Installation",
+          autogenerate: { directory: "installation" },
+        },
+        {
+          label: "Core Concepts",
+          autogenerate: { directory: "core-concepts" },
+        },
+        {
+          label: "CLI",
+          autogenerate: { directory: "cli" },
+        },
+        {
+          label: "Infrastructure",
+          autogenerate: { directory: "infrastructure" },
+        },
+        {
+          label: "Analysis",
+          autogenerate: { directory: "analysis" },
+        },
+        {
+          label: "Plugins",
+          autogenerate: { directory: "plugins" },
+        },
+        {
+          label: "Tutorials",
+          autogenerate: { directory: "tutorials" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Contributing",
+          autogenerate: { directory: "contributing" },
+        },
+        {
+          label: "Development",
+          autogenerate: { directory: "development" },
+        },
+      ],
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/DualHorizon/malbox' },
+        { icon: 'twitter', label: 'Twitter', href: "https://x.com/malbox_sandbox/" },
+      ],
+      plugins: [
+        starlightThemeBlack({
+          navLinks: [
+            {
+              // optional
+              label: "Docs",
+              link: "getting-started/introduction",
+            },
+          ],
+          //optional
+          footerText: "",
+        }),
+      ],
+      components: {
+        Hero: "./src/overrides/Hero.astro",
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true }]],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
